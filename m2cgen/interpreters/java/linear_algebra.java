779ffcd08667e6e47cac4d76540630c246b4b0af
chris.smith <chris.smith@zocdoc.com>
2020-09-08 09:27:17 -0400

    Change the categorical lookup to use a BitSet instead of HashSet. This is ~3x faster at the cost of some additional memory

--- m2cgen/interpreters/java/linear_algebra.java
+++ m2cgen/interpreters/java/linear_algebra.java
@@ -19,15 +19,47 @@ public static double[] mulVectorNumber(double[] v1, double num) {
 }
 
 
-public static boolean contains(HashSet<Integer> v1, double featureRef) {
+public static boolean contains(MinBitset v1, double featureRef) {
     return v1.contains((int) featureRef);
 }
 
-public static HashSet<Integer> parseIntArray(String input) {
+public static MinBitset parseIntArray(String input) {
     String[] strings = input.split(",");
-    HashSet<Integer> numbers = new HashSet<Integer>(strings.length);
+    int[] numbers = new int[strings.length];
     for (int i = 0; i < strings.length; i++) {
-        numbers.add(Integer.parseInt(strings[i]));
+        numbers[i] = Integer.parseInt(strings[i]);
     }
-    return numbers;
+    return new MinBitset(numbers);
+}
+
+
+public static class MinBitset {
+
+    private final int offset;
+    private final BitSet set;
+
+    // Inspired by TreeLite which uses bitmaps to deal with categorical variables. This is ~3x faster vs HashSet<Integer>
+    // https://github.com/dmlc/treelite/blob/46c8390aed4491ea97a017d447f921efef9f03ef/src/compiler/common/categorical_bitmap.h#L22-L28
+    public MinBitset(int[] input) {
+        Arrays.sort(input);
+
+        // offset the values by the minimum. this helps with memory usage for ranges that start high
+        offset = input[0];
+        int max = input[input.length - 1];
+
+        set = new BitSet(max - offset);
+
+        for (int value : input) {
+            int categorical = value - offset;
+            set.set(categorical);
+        }
+    }
+
+    public boolean contains(int value) {
+        value -= offset;
+        if (value > -1) {
+            return set.get(value);
+        }
+        return false;
+    }
 }
